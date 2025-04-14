89d0233db4767b552cfa9401dfcad32a4cbd50b9
Diego Meneses <di.menesesv@duocuc.cl>
2025-04-14 04:17:26 -0400

    commit inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.proscout',
+  appName: 'proscout',
+  webDir: 'www'
+};
+
+export default config;
