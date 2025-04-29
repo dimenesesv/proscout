b3b61edf5e2c1cbf2b4ac334451c5a6f862c2176
dimenesesv <di.menesesv@duocuc.cl>
2025-04-28 20:33:19 -0400

    Primer commit - Proyecto Ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proscout',
+  webDir: 'www'
+};
+
+export default config;
