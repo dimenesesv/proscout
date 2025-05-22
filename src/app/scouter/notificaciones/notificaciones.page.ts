be4305db80203dc85d63e2dbec5c00a6e82192df
Sebastian R <s.rebolledos@duocuc.cl>
2025-05-22 00:55:17 -0400

    creacion de notificaciones, falta llamar el servicio en donde ocurra el evento; aceptar solicitud, vista perfil, subir nueva foto

--- src/app/scouter/notificaciones/notificaciones.page.ts
+++ src/app/scouter/notificaciones/notificaciones.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
 
 @Component({
   selector: 'app-notificaciones',
@@ -6,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notificaciones.page.scss'],
   standalone: false,
 })
+
 export class NotificacionesPage implements OnInit {
+  notificaciones: any[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.cargarNotificaciones();
   }
 
+  async cargarNotificaciones() {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (!user) return;
+
+    const db = getFirestore();
+    const notificacionesRef = collection(db, 'notificaciones');
+    const q = query(notificacionesRef, where('uidScouter', '==', user.uid));
+
+    const snapshot = await getDocs(q);
+    this.notificaciones = snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+  }
 }
